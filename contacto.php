bda3672ecf03a093bae8798d35b5e1869c158a72
Cristian Oscco Cañari <cristiandaos@icloud.com>
2023-11-11 02:30:19 -0500

    Add files via upload

--- contacto.php
+++ contacto.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <br><br>
+    <br>
     <div class="buttons">
         <span class="bt-ba">Sede Barrio Chino</span>
         <span class="bt-mi">Sede Miraflores</span>
@@ -18,7 +18,6 @@
         <div class="contact-in">
             <h1>Visítanos</h1>
             <p><i class="fa-solid fa-location-dot"></i>Jr. Paruro 878, Lima 15001</p>
-            <p><i class="fa-regular fa-clock"></i>Lun. a Sáb. (9:30 AM - 9:15 PM) - Domingos y Feriados (9:30 AM - 8:00 PM)</p>
             <p><i class="fa-regular fa-envelope"></i>correo@walok.com.pe</p>
             <p><i class="fa-solid fa-phone"></i>(01) 6845127</p>
             <p><i class="fa-brands fa-whatsapp"></i>(+51) 988888888 <b>- Sólo para delivery</b></p>
@@ -32,18 +31,92 @@
         <div class="contact-in">
             <h1>Visítanos</h1>
             <p><i class="fa-solid fa-location-dot"></i>Av. Angamos 700, Miraflores 15074</p>
-            <p><i class="fa-regular fa-clock"></i>Lun. a Sáb. (9:30 AM - 9:15 PM) - Domingos y Feriados (9:30 AM - 8:00 PM)</p>
             <p><i class="fa-regular fa-envelope"></i>correo@walok.com.pe</p>
             <p><i class="fa-solid fa-phone"></i>(01) 6845127</p>
             <p><i class="fa-brands fa-whatsapp"></i>(+51) 988888888 <b>- Sólo para delivery</b></p>
         </div>  
     </div>
+    
     <h2 class="title-clients">Opinión de nuestros clientes</h2>
     <div class="wrapper">
         <div class="testimonial-container" id="testimonial-container"></div>
         <button id="prev">&lt;</button>
         <button id="next">&gt;</button>
       </div><br>
+
+      <div class="row staff">
+            <div class="col" style="max-width: 500px; text-align:center; margin: 0 auto;">
+            <h4 style="color: #fff;"><strong>Horario de atención</strong></h4>
+                       
+                <div class="signup-form">
+                    <form action="#footer" method="post">
+                        <div class="form-group">
+                            <label style="color: #fff;">Ingresa una fecha</label>
+                            <input type="date" class="form-control" name="date" placeholder="Date" required="required">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="check_schedule" class="btn btn-dark btn-block">Consultar horario de apertura</button>
+                        </div>
+                    </form>     
+<?php
+
+if(isset($_POST['check_schedule'])){
+      
+require 'includes/dbh.inc.php';
+            
+$date= $_POST['date'];
+ 
+    $sql = "SELECT * FROM schedule WHERE date = '$date'";
+    $result = $conn->query($sql);
+    if ($result->num_rows == 1) {
+        while($row = $result->fetch_assoc()) {
+            echo"
+                <table class='table table-sm table-striped table-dark text-center'>
+                   <thead>
+                    <tr>
+                    <th scope='col'>Fecha</th>
+                    <th scope='col'>Hora de atención</th>
+                    <th scope='col'>Hora de cierre</th>
+                    </tr>
+                   </thead>
+                   <tbody>
+                    <tr>
+                    <th scope='row'><em>". $date . "</em></th>
+                    <td>".$row['open_time']."</td>
+                    <td>".$row['close_time']."</td>
+                    </tr>
+                   </tbody>
+                </table>";
+                }
+            }
+        else{
+         echo"
+                <table class='table table-striped table-dark text-center'>
+                   <thead>
+                    <tr>
+                    <th scope='col'>Fecha</th>
+                    <th scope='col'>Hora de atención</th>
+                    <th scope='col'>Hora de cierre</th>
+                    </tr>
+                   </thead>
+                   <tbody>
+                    <tr>
+                    <th scope='row'><em>". $date . "</em></th>
+                    <td>09:15:00</td>
+                    <td>22:00:00</td>
+                    </tr>
+                   </tbody>
+                </table>";
+            }
+         
+   //cerrar conexión
+   mysqli_close($conn);
+}
+?>
+                        
+                </div><br>
+            </div>
+      
     <script src="js/script.js"></script>
     <footer class="footer">
         <div class="g1">
